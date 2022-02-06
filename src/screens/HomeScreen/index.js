bb5392fe90a9050687cba63fbd84864264175792
kleitong1 <44074264+kleitong1@users.noreply.github.com>
2022-02-06 16:10:28 -0300

    att

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,4 +1,4 @@
-import Footer from '../../components/patterns/Footer'
+import Footer from '../../components/patterns/footer'
 import Link from '../../components/Link'
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
